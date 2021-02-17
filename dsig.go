d1eeeb41d9cce0192e468a71480d52c1ae981f92
Valerii Lider <valerii.lider@gmail.com>
2021-02-18 00:48:52 +0100

    added Sign function, removed some interfaces as we don't use them

--- dsig.go
+++ dsig.go
@@ -2,38 +2,42 @@ package dsig
 
 import (
 	"crypto"
-	"crypto/x509"
 	"encoding/base64"
 
+	"github.com/noshto/dsig/pkg/safenet"
 	"github.com/noshto/dsig/pkg/signedxml"
+	"github.com/noshto/sep"
 
 	"github.com/beevik/etree"
 )
 
-// CertificateProvider defines an interface that provides x509 certificate
-type CertificateProvider interface {
-	X509() (x509.Certificate, error)
+// Params represents set of parameters needed for calling Sign function
+type Params struct {
+	SepConfig     *sep.Config
+	SafenetConfig *safenet.Config
+	InFile        string
+	OutFile       string
 }
 
-// PKCS1v15Signer defines an extension for crypto.Signer which also supports pkcs1v15 signature
-type PKCS1v15Signer interface {
-	crypto.Signer
-	SignPKCS1v15(data []byte) ([]byte, error)
-}
+// Sign signs given xml
+func Sign(params *Params) error {
 
-// Signer represents an interface of signer
-type Signer struct {
-	Signer      PKCS1v15Signer
-	Certificate CertificateProvider
-}
+	doc := etree.NewDocument()
+	if err := doc.ReadFromFile(params.InFile); err != nil {
+		return err
+	}
 
-// Sign signs given xml. Prerequisites: xml document should be completely formed, eg signature-related elements should exists
-func (s *Signer) Sign(doc *etree.Document) (*etree.Document, error) {
+	// Initialize Signer
+	signer := &safenet.SafeNet{}
+	if err := signer.Initialize(params.SafenetConfig); err != nil {
+		return err
+	}
+	defer signer.Finalize()
 
 	// Fill in certificate and subject of signature element
-	cert, err := s.Certificate.X509()
+	cert, err := signer.GetCertificate()
 	if err != nil {
-		return nil, err
+		return err
 	}
 
 	x509Cert := doc.FindElement("//X509Certificate")
@@ -48,22 +52,25 @@ func (s *Signer) Sign(doc *etree.Document) (*etree.Document, error) {
 	// sign
 	str, err := doc.WriteToString()
 	if err != nil {
-		return nil, err
+		return err
 	}
-	var cSigner crypto.Signer = s.Signer.(crypto.Signer)
-	signer, err := signedxml.NewSigner(str, &cSigner)
+	cSigner := crypto.Signer(signer)
+	xmlSigner, err := signedxml.NewSigner(str, &cSigner)
 	if err != nil {
-		return nil, err
+		return err
 	}
-	signer.SetReferenceIDAttribute("Id")
-	str, err = signer.Sign()
+	xmlSigner.SetReferenceIDAttribute("Id")
+	str, err = xmlSigner.Sign()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	doc = etree.NewDocument()
-	err = doc.ReadFromString(str)
-	if err != nil {
-		return nil, err
+	if err = doc.ReadFromString(str); err != nil {
+		return err
 	}
-	return doc, nil
+
+	doc.IndentTabs()
+	doc.Root().SetTail("")
+
+	return doc.WriteToFile(params.OutFile)
 }
